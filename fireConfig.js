f082bf08151abd92f885f25dcaf47248b83d7bb3
Dudaccampos <132668173+Dudaccampos@users.noreply.github.com>
2024-04-04 23:52:03 -0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA3qp7dNVF-apsP8Iya_ASvlvrNJjyZ3hU",
+    authDomain: "crudcomfirebase-54cea.firebaseapp.com",
+    databaseURL: "https://crudcomfirebase-54cea-default-rtdb.firebaseio.com",
+    projectId: "crudcomfirebase-54cea",
+    storageBucket: "crudcomfirebase-54cea.appspot.com",
+    messagingSenderId: "340803845354",
+    appId: "1:340803845354:web:999c03fd06adbbf201ab93"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
